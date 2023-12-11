2af3c64ee0c39380510ad8e9e0417dafb823cdf8
FSevencan <fatihsevencan61@gmail.com>
2023-12-11 15:30:24 +0300

    chore: initial commit from nArchitecture.Gen

---
+++ src/tobetoPlatformCleanArchitecture/Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Core.Security.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+namespace Persistence.Contexts;
+
+public class BaseDbContext : DbContext
+{
+    protected IConfiguration Configuration { get; set; }
+    public DbSet<EmailAuthenticator> EmailAuthenticators { get; set; }
+    public DbSet<OperationClaim> OperationClaims { get; set; }
+    public DbSet<OtpAuthenticator> OtpAuthenticators { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+
+    public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration)
+        : base(dbContextOptions)
+    {
+        Configuration = configuration;
+        Database.EnsureCreated();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+}
---
+++ src/tobetoPlatformCleanArchitecture/Persistence/EntityConfigurations/OperationClaimConfiguration.cs
@@ -0,0 +1,38 @@
+using Application.Features.OperationClaims.Constants;
+using Core.Security.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.EntityConfigurations;
+
+public class OperationClaimConfiguration : IEntityTypeConfiguration<OperationClaim>
+{
+    public void Configure(EntityTypeBuilder<OperationClaim> builder)
+    {
+        builder.ToTable("OperationClaims").HasKey(oc => oc.Id);
+
+        builder.Property(oc => oc.Id).HasColumnName("Id").IsRequired();
+        builder.Property(oc => oc.Name).HasColumnName("Name").IsRequired();
+        builder.Property(oc => oc.CreatedDate).HasColumnName("CreatedDate").IsRequired();
+        builder.Property(oc => oc.UpdatedDate).HasColumnName("UpdatedDate");
+        builder.Property(oc => oc.DeletedDate).HasColumnName("DeletedDate");
+
+        builder.HasQueryFilter(oc => !oc.DeletedDate.HasValue);
+
+        builder.HasMany(oc => oc.UserOperationClaims);
+
+        builder.HasData(getSeeds());
+    }
+
+    private HashSet<OperationClaim> getSeeds()
+    {
+        int id = 0;
+        HashSet<OperationClaim> seeds =
+            new()
+            {
+                new OperationClaim { Id = ++id, Name = GeneralOperationClaims.Admin }
+            };
+
+        return seeds;
+    }
+}
