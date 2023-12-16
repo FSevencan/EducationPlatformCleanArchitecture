4f1eb4b02f83a072d4da5ca6a0b42882dda14f53
kadiiravsaar <kadiiravsar@gmail.com>
2023-12-16 16:08:22 +0300

    entities revise

--- src/tobetoPlatformCleanArchitecture/Domain/Entities/Course.cs
+++ src/tobetoPlatformCleanArchitecture/Domain/Entities/Course.cs
@@ -3,9 +3,10 @@
 namespace Domain.Entities;
 public class Course : Entity<Guid>
 {    
-    public TimeSpan? TotalTime { get; set; }
+    public int TotalTime { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-
-    public  ICollection<SectionCourse>? SectionCourses { get; set; }    
+    public ICollection<Lesson> Lessons { get; set; }
+    public ICollection<SectionCourse>? SectionCourses { get; set; }
 }
+
--- src/tobetoPlatformCleanArchitecture/Domain/Entities/SectionAbout.cs
+++ src/tobetoPlatformCleanArchitecture/Domain/Entities/SectionAbout.cs
@@ -9,7 +9,7 @@ public class SectionAbout : Entity<Guid>
     public string? Text { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
-    public TimeSpan? EstimatedDuration { get; set; }  
+    public DateTime EstimatedDuration { get; set; }  
 
     public  Section Section { get; set; }
     public  ProducerCompany ProducerCompany { get; set; }
