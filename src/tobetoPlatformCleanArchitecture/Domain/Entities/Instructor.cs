9ea79a727cfd39529514c8bb22c38e687b0b3e7d
kadirozdemir <kadirozdemirr00@gmail.com>
2023-12-22 19:24:30 +0300

    Instructor Entity Revised

--- src/tobetoPlatformCleanArchitecture/Domain/Entities/Instructor.cs
+++ src/tobetoPlatformCleanArchitecture/Domain/Entities/Instructor.cs
@@ -7,14 +7,14 @@ public class Instructor : Entity<Guid>
     public int UserId { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string ImageUrl { get; set; }
+    public string? ImageUrl { get; set; }
     public string Email { get; set; }
-    public DateTime BirthDate { get; set; }
-    public string PhoneNumber { get; set; }
-    public string About { get; set; }
-    public string Title { get; set; }
+    public DateTime? BirthDate { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? About { get; set; }
+    public string? Title { get; set; }
 
-    public User User { get; set; }
+    public User? User { get; set; }
 
     public ICollection<SectionInstructor>? SectionInstructors { get; set; }
 }
