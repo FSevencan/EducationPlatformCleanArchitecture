c4664fb37a8d1516c16fa4b24e3f89c84805e0ec
kadirozdemir <kadirozdemirr00@gmail.com>
2024-01-19 04:30:42 +0300

    InstructorResponse Updated

--- src/tobetoPlatformCleanArchitecture/Application/Features/Instructors/Queries/GetById/GetByIdInstructorResponse.cs
+++ src/tobetoPlatformCleanArchitecture/Application/Features/Instructors/Queries/GetById/GetByIdInstructorResponse.cs
@@ -8,17 +8,20 @@ namespace Application.Features.Instructors.Queries.GetById;
 public class GetByIdInstructorResponse : IResponse
 {
     public int Id { get; set; }
-    public int UserId { get; set; }
+    public int UserId { get; set; } 
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string ImageUrl { get; set; }
     public string Email { get; set; }
     public DateTime BirthDate { get; set; }
     public string PhoneNumber { get; set; }
-    //public string About { get; set; } // tabloda yok mu? Kontrol et !
+    public string? Biography { get; set; }
+    public string? GithubUrl { get; set; }
+    public string? LinkedinUrl { get; set; }
+    public string? TwitterUrl { get; set; }
     public string Title { get; set; }
 
+
     public ICollection<GetListInstructorsSectionListDto> Sections { get; set; }
-
-
+   
 }
