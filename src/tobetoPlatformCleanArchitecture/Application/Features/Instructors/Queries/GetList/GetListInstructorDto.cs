891dbfd4f425b2737885f2320fd6c5c796d234e2
Barış YILDIRIM <77466278+brsyldrm@users.noreply.github.com>
2024-01-29 20:08:31 +0300

    sectiondetails

--- src/tobetoPlatformCleanArchitecture/Application/Features/Instructors/Queries/GetList/GetListInstructorDto.cs
+++ src/tobetoPlatformCleanArchitecture/Application/Features/Instructors/Queries/GetList/GetListInstructorDto.cs
@@ -7,5 +7,10 @@ namespace Application.Features.Instructors.Queries.GetList
         public int Id { get; set; }
         public string Name { get; set; }
         public string? ImageUrl { get; set; }
+        public string? Title { get; set; }
+        public string? Biography { get; set; }
+
+
+
     }
 }
